801a5cbe24d4e8c3c12e5b5aa3720cddcfc67182
barry3232 <davelanista@gmail.com>
2024-02-24 05:14:18 -0800

    hello baryry

---
+++ bin/barry.dart
@@ -0,0 +1,35 @@
+void main(){
+
+  // helloBarryShirt
+  const r = 12;
+  var d = 23;
+  int c = 15;
+
+  String dave = 'boom';
+  bool red = false;
+  double g = 4.03;
+
+
+  List<int> yes = [1,2,3];
+
+  print(yes);
+
+  List<String> letam = ['dear','him','her'];
+  print(letam);
+
+  List <String> jennifer = ['love','she','heat'];
+  print (jennifer);
+
+  List<bool> wrong = [true,false];
+  print(wrong);
+
+  List<bool> james = [false, true];
+  print(james);
+
+  List<double> david = [3.4,4.3,6.60];
+  print(david);
+
+
+}
+
+
