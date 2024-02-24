8151f4e84efc2b210ab499b0db26d96a456f7b7a
barry3232 <davelanista@gmail.com>
2024-02-24 07:11:41 -0800

    hello calculator

--- bin/barry.dart
+++ bin/barry.dart
@@ -1,21 +1,39 @@
 import 'dart:io';
 
-void main(){
-
+void main() {
   print('please enter a number');
   String? number = stdin.readLineSync();
-  if (number != null){
-    print('please enter a second number');
-    String? secondNumber = stdin.readLineSync();
-    if (secondNumber != null){
-      int a = int.parse(number);
-      int b = int.parse(secondNumber);
-      int c = a+b;
-      print('Your answer is $c ');
+  if (number != null) {
+    print('please choose one of this sign +,_,*');
+    String? sign = stdin.readLineSync();
+    if (sign != null) {
+      if (sign == '+') {
+        print('Please enter a second number');
+        String? secondNumber = stdin.readLineSync();
+        if (secondNumber != null) {
+          int first = int.parse(number);
+          int second = int.parse(secondNumber);
+
+          int answerPlus = first + second;
+
+          print('Your answer is $answerPlus');
+        }
+      } else if (sign == '-') {
+        print('Please enter a second number');
+        String? secondNumber = stdin.readLineSync();
+        if (secondNumber != null) {
+          int first = int.parse(number);
+          int second = int.parse(secondNumber);
+
+          int answerMinus = first - second;
+
+          print('Your answer is $answerMinus');
+        }
+      }else {
+        print('we do not know that function');
+      }
     }
-
   }
-
 }
 
 
