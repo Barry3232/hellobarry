e55fcd940a5d1ecc3eb01b549e0c9b7d77b1f5c7
barry3232 <davelanista@gmail.com>
2024-03-01 13:26:02 -0800

    my work

--- bin/barry.dart
+++ bin/barry.dart
@@ -1,15 +1,9 @@
 import 'dart:io';
 
-void main(){
-  print('pls enter your email');
-  String? email = stdin.readLineSync();
-  if (email != null) {
-    print('please what action to take next open mail, read mail, log out of mail');
-    String? action = stdin.readLineSync();
-    if (action != null) {
-      if (action == 'open mail') {
-        print('mail is been opened');
-      }
-    }
-  }
+void main() {
+  for (int b = 1; b <= 5; b++) {
+    print ('hello ${b + 1}');
+
+  }else ()
 }
+
